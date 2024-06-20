36eb5075c062d64171d13e2f9ca8c46837d2b041
Brandon Chen <zipengchen1832@gmail.com>
2024-06-20 15:52:25 -0400

    day 8

--- basic/day8_mvc/features/todo/todoController.js
+++ basic/day8_mvc/features/todo/todoController.js
@@ -4,12 +4,27 @@ class TodoController {
   constructor(model, view) {
     this.#model = model;
     this.#view = view;
+    this.initApp();
+  }
+
+  initApp() {
     this.setUpEvents();
+    this.fetchTodos();
   }
 
   setUpEvents() {
     this.setUpDeleteEvent();
     this.setUpAddEvent();
+  }
+
+  fetchTodos() {
+    todoAPI.fetchTodosAPI().then((todos) => {
+      this.#model.setTodos(todos);
+      this.#view.setRemainingCount(this.#model.length);
+      todos.forEach((todo) => {
+        this.#view.renderTodoElement(todo);
+      });
+    });
   }
 
   setUpAddEvent() {
@@ -19,18 +34,11 @@ class TodoController {
         task: this.#view.addTodoInput.value,
       };
 
-      fetch("http://localhost:3000/todos", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newTodo),
-      })
-        .then((res) => res.json())
-        .then((_newTodo) => {
-          this.#view.renderTodoElement(_newTodo);
-          this.#model.addTodo(_newTodo);
-        });
+      todoAPI.postTodoAPI(newTodo).then((_newTodo) => {
+        this.#model.addTodo(_newTodo);
+        this.#view.setRemainingCount(this.#model.length);
+        this.#view.renderTodoElement(_newTodo);
+      });
     });
   }
 
@@ -38,7 +46,12 @@ class TodoController {
     this.#view.todoList.addEventListener("click", (e) => {
       if (e.target.classList.contains("todo-item__delete")) {
         const todoId = e.target.parentElement.getAttribute("id");
-        this.#view.removeTodoElement(todoId);
+
+        todoAPI.deleteTodoAPI(todoId).then(() => {
+          this.#model.removeTodo(todoId);
+          this.#view.setRemainingCount(this.#model.length);
+          this.#view.removeTodoElement(todoId);
+        });
       }
     });
   }
