c976367f13702ab8d26e4d36714447c1ed7a1f43
Brandon Chen <zipengchen1832@gmail.com>
2024-07-03 12:33:55 -0400

    training day 1

---
+++ advance/react-training/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./App.css";
+import Day1React from "./Day1";
+
+console.log(React);
+
+function App() {
+  return (
+    <>
+      <h1>Advanced React Training</h1>
+      <Day1React />
+    </>
+  );
+}
+
+export default App;
