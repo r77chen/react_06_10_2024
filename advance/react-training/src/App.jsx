17907bf0f3b84e58608454dbd42ed2a834c7283e
Brandon Chen <zipengchen1832@gmail.com>
2024-07-08 15:15:21 -0400

    day 3 people list

--- advance/react-training/src/App.jsx
+++ advance/react-training/src/App.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import "./App.css";
 import Day1React from "./Day1";
 import RenderingTheUI from "./RenderingTheUI";
+import Day3 from "./Day3";
 
-// console.log(React);
 
 function App() {
   return (
     <>
       <h1>Advanced React Training</h1>
       {/* <Day1React /> */}
-      <RenderingTheUI />
+      {/* <RenderingTheUI /> */}
+      <Day3 />
     </>
   );
 }
