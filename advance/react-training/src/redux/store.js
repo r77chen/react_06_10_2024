7dc025450f7d36d7e580317cc14dcb2da21ab811
Brandon Chen <zipengchen1832@gmail.com>
2024-07-18 15:22:06 -0400

    minor update

--- advance/react-training/src/redux/store.js
+++ advance/react-training/src/redux/store.js
@@ -2,15 +2,6 @@ import { applyMiddleware, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 
-const logger = (store) => (next) => (action) => {
-  console.group(action.type);
-  console.info("dispatching", action);
-  let result = next(action);
-  console.log("next state", store.getState());
-  console.groupEnd();
-  return result;
-};
-
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
