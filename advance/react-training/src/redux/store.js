a0bf9a1834fb499206ae4d52e9cd13d54dd534f8
Brandon Chen <zipengchen1832@gmail.com>
2024-07-18 15:40:39 -0400

    implemented redux persist

--- advance/react-training/src/redux/store.js
+++ advance/react-training/src/redux/store.js
@@ -1,7 +1,18 @@
 import { applyMiddleware, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(thunk));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
